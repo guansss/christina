026c0c6afde26f63573460cbac3f5ea81d22f926
Guan <821143943@qq.com>
2021-02-17 13:38:38 +0800

    Handle server errors properly

--- christina/server/server.py
+++ christina/server/server.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, staticfiles
+from fastapi import FastAPI, staticfiles, HTTPException
+from fastapi.exception_handlers import http_exception_handler
 from fastapi.middleware.cors import CORSMiddleware
 import christina.env
 from . import video, download
@@ -6,19 +7,29 @@ import os
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=list(filter(None, os.environ['CORS_ORIGINS'].splitlines())),
-    allow_methods=["GET", 'POST'],
-    allow_headers=["*"],
-)
-
 app.mount("/static", staticfiles.StaticFiles(directory=os.environ['DATA_DIR']), name="static")
 
 app.include_router(video.router)
 app.include_router(download.router)
 
 
+@app.exception_handler(Exception)
+async def general_handler(request, exc):
+    if not isinstance(exc, HTTPException):
+        # convert any Exception to an HTTPException
+        exc= HTTPException(500, repr(exc))
+
+    return await http_exception_handler(request,exc)
+
+
 @app.get('/')
 def index():
     return 'Hello Christina'
+
+# wrap the entire app so CORSMiddleware can handle non-HTTPExceptions
+app = CORSMiddleware(
+    app=app,
+    allow_origins=list(filter(None, os.environ['CORS_ORIGINS'].splitlines())),
+    allow_methods=["GET", 'POST'],
+    allow_headers=["*"],
+)
